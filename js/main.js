b0550d9862911bf33264e54b4852705f8e919c45
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2022-10-16 13:43:23 -0700

    cambio Total
    Nueva versión mejorada puesta a punto.

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="direct">Mexicano</i>',
+		'<i class="direct">Hidrocalido</i>',
+		'<i class="direct">Ranchero</i>',
+		'<i class="direct">Hola Mundo ;)</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+const typed2 = new Typed('.typed2', {
+	strings: [
+		'<i class="direct">Plataforma</i>',
+		'<i class="direct">Documentacion</i>',
+		'<i class="direct">Temas de JS</i>',
+		'<i class="direct">Python</i>',
+		'<i class="direct">C#</i>',
+		'<i class="direct">Java</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 30, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 60, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+const typed3 = new Typed('.typed3', {
+	strings: [
+		'<i class="direct">Linkedln</i>',
+		'<i class="direct">GitHub</i>',
+		'<i class="direct">Instagram</i>',
+		'<i class="direct">Twitter</i>',
+		'<i class="direct">Google Developers</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 30, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 60, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
